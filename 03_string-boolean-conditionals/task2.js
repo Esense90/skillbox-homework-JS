c1d16fcb8365c08b7484e8629237194e0f6f34b4
Kirill <kirillryabushev@gmail.com>
2021-10-24 18:59:04 +1100

    add task2

--- 03_string-boolean-conditionals/task2.js
+++ 03_string-boolean-conditionals/task2.js
@@ -1,12 +1,13 @@
-let name = 'кОНстаНТИн';
+let name = 'кОНстанТин';
 
 let fearstLetterName = name.substr(0, 1);
 
 let nextLetterName = name.substr(1);
 
-// if (fearstLetterName == fearstLetterName.toUpperCase()) {
-//   console.log('true')
-// } else(fearstLetterName == fearstLetterName.toLowerCase()); {
-//   let regLetterName = fearstLetterName.toUpperCase();
-//   console.log(regLetterName + nextLetterName)
-// }
+if (fearstLetterName == fearstLetterName.toUpperCase() && nextLetterName == nextLetterName.toLowerCase()) {
+
+} else(fearstLetterName == fearstLetterName.toLowerCase()); {
+  let regLetterName = fearstLetterName.toUpperCase();
+  let regNextLettername = nextLetterName.toLowerCase();
+  console.log(regLetterName + regNextLettername);
+}
