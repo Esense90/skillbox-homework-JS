20812a284915c180e8751c36a916d77f0e112bed
Kirill <kirillryabushev@gmail.com>
2021-10-24 18:22:09 +1100

    hw

--- 03_string-boolean-conditionals/task2.js
+++ 03_string-boolean-conditionals/task2.js
@@ -1,11 +1,12 @@
 let name = 'кОНстаНТИн';
 
-let surname = 'ваСИЛЬевиЧ';
-
 let fearstLetterName = name.substr(0, 1);
 
 let nextLetterName = name.substr(1);
 
-if (fearstLetterName = fearstLetterName.toLowerCase() === fearstLetterName) {
-    return fearstLetterName;
-} else console.log('Lose')
+// if (fearstLetterName == fearstLetterName.toUpperCase()) {
+//   console.log('true')
+// } else(fearstLetterName == fearstLetterName.toLowerCase()); {
+//   let regLetterName = fearstLetterName.toUpperCase();
+//   console.log(regLetterName + nextLetterName)
+// }
