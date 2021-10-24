0667c255c49498ed574ef7f5cf9df66c02bf9c83
Kirill <kirillryabushev@gmail.com>
2021-10-24 19:54:25 +1100

    cancel home work 03

--- 03_string-boolean-conditionals/task2.js
+++ 03_string-boolean-conditionals/task2.js
@@ -1,13 +1,27 @@
-let name = 'кОНстанТин';
+let name = 'консТАНтиН';
+let surname = 'оЛегОВич'
 
+// Получаем 1 и последующие строки
 let fearstLetterName = name.substr(0, 1);
-
 let nextLetterName = name.substr(1);
 
-if (fearstLetterName == fearstLetterName.toUpperCase() && nextLetterName == nextLetterName.toLowerCase()) {
+let fearstLetterSur = surname.substr(0, 1);
+let nextLetterSur = surname.substr(1);
 
-} else(fearstLetterName == fearstLetterName.toLowerCase()); {
-  let regLetterName = fearstLetterName.toUpperCase();
-  let regNextLettername = nextLetterName.toLowerCase();
-  console.log(regLetterName + regNextLettername);
+// Преобразуем строки в нужный регистр
+let regLetterName = fearstLetterName.toUpperCase();
+let regNextLetterName = nextLetterName.toLowerCase();
+let regName = regLetterName + regNextLetterName;
+
+let regLetterSur = fearstLetterSur.toUpperCase();
+let regNextLetterSur = nextLetterSur.toLowerCase();
+let regSur = regLetterSur + regNextLetterSur;
+
+console.log(regName, regSur)
+
+//  Сравниваем и выводим значение "true" или "false"
+if (regName !== name && regSur !== surname) {
+  console.log('Имя было изменено')
+} else {
+  console.log('Имя осталось без изменений')
 }
