eb7144fc5d45ad96484b38f4c394d64700a23e7c
@kirill_riabyshev <you@example.com>
2021-10-24 16:16:28 +1100

    home work

---
+++ 03_string-boolean-conditionals/task2.js
@@ -0,0 +1,11 @@
+let name = 'кОНстаНТИн';
+
+let surname = 'ваСИЛЬевиЧ';
+
+let fearstLetterName = name.substr(0, 1);
+
+let nextLetterName = name.substr(1);
+
+if (fearstLetterName = fearstLetterName.toLowerCase() === fearstLetterName) {
+    return fearstLetterName;
+} else console.log('Lose')
