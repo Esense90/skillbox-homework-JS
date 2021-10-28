aa5724cb50979c2627663e26e9159d89b4fe77bb
Kirill <kirillryabushev@gmail.com>
2021-10-28 16:07:10 +1100

    cancel task3

--- 04_arrays-and-loops/task3.js
+++ 04_arrays-and-loops/task3.js
@@ -1,1 +1,15 @@
-let roadMines = [false, false, false, true, false, false, false, false, false, false];
+let roadMines = [false, false, false, true, false, false, false, true, false, false];
+
+let lives = 2;
+
+for (let a in roadMines) {
+  console.log(`Танк переместился на ${parseInt(a) + 1}`);
+  if (roadMines[a]) {
+    lives--;
+    if (lives < 1) {
+      console.log(`Танк уничтожен!`);
+      break;
+    }
+    console.log(`Танк повреждён!`);
+  }
+}
