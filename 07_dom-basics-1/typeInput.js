07fc5caca64cccfbfe1bc784abbd5ba145bfd142
Kirill <kirillryabushev@gmail.com>
2021-11-05 22:13:03 +1100

    start task2

---
+++ 07_dom-basics-1/typeInput.js
@@ -0,0 +1,15 @@
+let newInput = document.createElement("input");
+newInput.setAttribute('id', 'new-input');
+document.body.append(newInput);
+newInput.style.marginLeft = "200px";
+newInput.style.marginRight = "50px";
+
+let newH2 = document.createElement("h2");
+newH2.setAttribute('id', 'new-h2');
+document.body.append(newH2);
+newH2.style.minWidth = "50px"
+newH2.style.height = "25px"
+newH2.style.border = "1px solid black";
+
+let inp = document.getElementById('new-input');
+let h2 = document.getElementById('new-h2');
