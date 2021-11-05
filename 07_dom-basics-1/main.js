8516e658ae998b4e289052a0fb52e69e9f9ff29b
Kirill <kirillryabushev@gmail.com>
2021-11-05 13:57:31 +1100

    start 07

---
+++ 07_dom-basics-1/main.js
@@ -0,0 +1,22 @@
+let input = document.createElement("input");
+document.body.append(input);
+let div = document.createElement("div");
+div.textContent = ("0");
+div.innerHTML
+input.appendChild(div);
+let button = document.createElement("button");
+button.className = "btn";
+button.innerHTML = "Start-Stop";
+document.body.append(button);
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('.btn').addEventListener("click", timer);
