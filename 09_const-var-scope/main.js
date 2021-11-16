5b9cca4c093fd624c67cc7fa20d254dbc4e7c468
@kirill_riabyshev <you@example.com>
2021-11-16 16:01:55 +1100

    09

--- 09_const-var-scope/main.js
+++ 09_const-var-scope/main.js
@@ -1,81 +1,99 @@
 (() => {
 
-  const cardsArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16];
+    const cardsArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16];
 
-  let array = [];
+    let array = [];
 
-  let counter = 0;
+    let counter = 0;
+    let tmpCard = [];
 
-  function createApp() {
+    function createApp() {
 
-    const wrapper = document.createElement('div');
-    wrapper.className = 'wrapper__cards';
-    wrapper.id = "wrapper";
-    document.getElementById('container').append(wrapper);
+        const wrapper = document.createElement('div');
+        wrapper.className = 'wrapper__cards';
+        wrapper.id = "wrapper";
+        document.getElementById('container').append(wrapper);
 
-    for (let i = 0; i < 16; i++) {
-      const card = document.createElement('div');
-      const cardFrontFace = document.createElement('div')
-      const cardBackFace = document.createElement('div');
+        for (let i = 0; i < 16; i++) {
+            const card = document.createElement('div');
+            const cardFrontFace = document.createElement('div')
+            const cardBackFace = document.createElement('div');
 
-      card.classList.add('card');
-      cardFrontFace.classList.add('card__front--face', 'card__face');
-      cardBackFace.classList.add('card__back--face', 'card__face');
+            card.classList.add('card');
+            cardFrontFace.classList.add('card__front--face', 'card__face');
+            cardBackFace.classList.add('card__back--face', 'card__face');
 
-      card.append(cardFrontFace);
-      card.append(cardBackFace);
-      document.getElementById('wrapper').append(card);
+            card.append(cardFrontFace);
+            card.append(cardBackFace);
+            document.getElementById('wrapper').append(card);
 
-      card.addEventListener('click', () => {
-        card.classList.add('is-flipped');
-        setTimeout(() => {
-          card.classList.remove('is-flipped');
-        }, 3000)
-        counter++
+            const name = 'Vasia';
 
-        if (counter === 2) {
-          const notFlipped = document.querySelectorAll('div:not(.is-flipped)');
-          for (const n of notFlipped) {
-            n.style.pointerEvents = 'none';
-          };
-        };
-      });
+            const func = () => {
+                return 'Привет' + name
+            }
+
+            func()
+
+            card.addEventListener('click', (event) => {
+                console.log(event.currentTarget)
+                console.log(counter)
+                if (card.classList.contains('is-founded')) return false;
+                if (counter >= 2) return false;
+                card.classList.add('is-flipped');
+                tmpCard.push(card);
+                if (tmpCard.length >= 2) {
+                    let tmpContent = [
+                        tmpCard[0].querySelector('.card__back--face').textContent,
+                        tmpCard[1].querySelector('.card__back--face').textContent
+                    ]
+                    if (Number(tmpContent[0]) == Number(tmpContent[1])) {
+                        tmpCard[0].classList.add('is-founded');
+                        tmpCard[1].classList.add('is-founded');
+                        counter = 0;
+                        tmpCard = [];
+                        return false;
+                    }
+                    setTimeout(() => {
+                        tmpCard.forEach((cardTogle) => {
+                            cardTogle.classList.remove('is-flipped');
+                            counter--;
+                        })
+                        tmpCard = [];
+                    }, 3000)
+                }
+                counter++;
+            });
+        }
+
+        function createNumber(cardsArray) {
+            const cardBackFaceNumber = document.querySelectorAll('.card__back--face')
+            for (i = 0; i < 16; i++) {
+                array.push(cardsArray[i]);
+                cardBackFaceNumber[i].textContent = array[i];
+            }
+        }
+        createNumber(cardsArray);
+
+
+
+        function shuffle() {
+            const shuffleCard = document.querySelectorAll('.card');
+            shuffleCard.forEach(shuffleCard => {
+                let randomPos = Math.floor(Math.random() * 12);
+                shuffleCard.style.order = randomPos;
+            })
+        }
+        shuffle();
+
+
+
+
 
 
 
     }
 
-    function createNumber(cardsArray) {
-      const cardBackFaceNumber = document.querySelectorAll('.card__back--face')
-      for (i = 0; i < 16; i++) {
-        array.push(cardsArray[i]);
-        cardBackFaceNumber[i].textContent = array[i];
-      }
-    }
-    createNumber(cardsArray);
-
-
-
-    function shuffle() {
-      const shuffleCard = document.querySelectorAll('.card');
-      shuffleCard.forEach(shuffleCard => {
-        let randomPos = Math.floor(Math.random() * 12);
-        shuffleCard.style.order = randomPos;
-      })
-    }
-    shuffle();
-
-
-
-
-
-
-
-
-
-
-
-  }
-
-  window.createApp = createApp;
+    createApp(document.getElementById('container'));
+    createApp(document.getElementById('container'));
 })();
