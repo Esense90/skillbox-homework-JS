4e0aa9931108a3bcda44bf86dd4298f44f8a2001
@kirill_riabyshev <you@example.com>
2021-11-22 23:32:59 +1100

    help

--- 10_type-transform/main.js
+++ 10_type-transform/main.js
@@ -1,59 +1,51 @@
 (() => {
 
-  const form = document.createElement('form');
-  const fName = document.createElement('input');
-  const dBirth = document.createElement('input');
-  const dLearning = document.createElement('input');
-  const faculty = document.createElement('input');
-  const button = document.createElement('button');
+    let studentsArray = [];
 
+    function createForm() {
+        const form = document.createElement('form');
+        const fName = document.createElement('input');
+        const dBirth = document.createElement('input');
+        const dLearning = document.createElement('input');
+        const faculty = document.createElement('input');
+        const button = document.createElement('button');
 
+        fName.type = 'text';
+        dBirth.type = 'text';
+        dLearning.type = 'number';
 
-  let studentsArray = [];
+        function focus() {
+            dBirth.addEventListener('focus', () => {
+                dBirth.type = 'date';
+                dBirth.addEventListener('blur', () => {
+                    dBirth.type = 'text';
+                })
+            })
+        }
+        focus();
 
+        fName.placeholder = 'ФИО студента';
+        dBirth.placeholder = 'Дата рождения';
+        dLearning.placeholder = 'Год начала обучения';
+        faculty.placeholder = 'Факультет';
 
+        form.classList = "form";
+        fName.classList = "full__name";
+        dBirth.classList = "date__birth";
+        dLearning.classList = "date__start-training";
+        faculty.classList = "faculty";
+        button.classList = "button";
 
+        button.textContent = "Добавить студента";
 
+        form.append(fName, dBirth, dLearning, faculty, button);
+        document.getElementById('formWrapper').append(form);
 
-
-
-  function createForm() {
-    fName.type = 'text';
-    dBirth.type = 'text';
-    dLearning.type = 'number';
-
-    function focus() {
-      dBirth.addEventListener('focus', () => {
-        dBirth.type = 'date';
-        dBirth.addEventListener('blur', () => {
-          dBirth.type = 'text';
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            form.reset();
         })
-      })
-    }
-    focus();
+    };
 
-    fName.placeholder = 'ФИО студента';
-    dBirth.placeholder = 'Дата рождения';
-    dLearning.placeholder = 'Год начала обучения';
-    faculty.placeholder = 'Факультет';
-
-    form.classList = "form";
-    fName.classList = "full__name";
-    dBirth.classList = "date__birth";
-    dLearning.classList = "date__start-training";
-    faculty.classList = "faculty";
-    button.classList = "button";
-
-    button.textContent = "Добавить студента";
-
-    form.append(fName, dBirth, dLearning, faculty, button);
-    document.getElementById('formWrapper').append(form);
-
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      form.reset();
-    })
-  };
-
-  createForm(document.getElementById('formWrapper'))
+    createForm(document.getElementById('formWrapper'))
 })();
