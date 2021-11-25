a0299ed4cbe56d51509a9916884e69b56b1dacc9
Kirill <kirillryabushev@gmail.com>
2021-11-25 16:16:05 +1100

    shw10

--- 10_type-transform/main.js
+++ 10_type-transform/main.js
@@ -1,61 +1,69 @@
 (() => {
 
-    let studentsArray = [];
+  let studentsArray = [];
 
-    function createForm() {
-        const form = document.createElement('form');
-        const fName = document.createElement('input');
-        const dBirth = document.createElement('input');
-        const dLearning = document.createElement('input');
-        const faculty = document.createElement('input');
-        const button = document.createElement('button');
+  function createForm() {
+    const form = document.createElement('form');
+    const fName = document.createElement('input');
+    const dBirth = document.createElement('input');
+    const dLearning = document.createElement('input');
+    const faculty = document.createElement('input');
+    const button = document.createElement('button');
 
-        fName.type = 'text';
-        dBirth.type = 'text';
-        dLearning.type = 'number';
+    fName.type = 'text';
+    dBirth.type = 'text';
+    dLearning.type = 'number';
 
-        function focus() {
-            dBirth.addEventListener('focus', () => {
-                dBirth.type = 'date';
-                dBirth.addEventListener('blur', () => {
-                    dBirth.type = 'text';
-                })
-            })
-        }
-        focus();
-
-        fName.placeholder = 'ФИО студента';
-        dBirth.placeholder = 'Дата рождения';
-        dLearning.placeholder = 'Год начала обучения';
-        faculty.placeholder = 'Факультет';
-
-        form.classList = "form";
-        fName.classList = "full__name";
-        dBirth.classList = "date__birth";
-        dLearning.classList = "date__start-training";
-        faculty.classList = "faculty";
-        button.classList = "button";
-
-        button.textContent = "Добавить студента";
-
-        form.append(fName, dBirth, dLearning, faculty, button);
-        document.getElementById('formWrapper').append(form);
-
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            form.reset();
+    function focus() {
+      dBirth.addEventListener('focus', () => {
+        dBirth.type = 'date';
+        dBirth.addEventListener('blur', () => {
+          dBirth.type = 'text';
         })
+      })
+    }
+    focus();
 
-        button.addEventListener('click', () => {
-            if (fName.value.length != 0 || dBirth.value.length != 0 || dLearning.value.length != 0 || faculty.value.length != 0) {
-                studentsArray.push(fName.value, dBirth.value, dLearning.value, faculty.value);
-            }
-            if (fName.value.length <= 0 || dBirth.value.length <= 0 || dLearning.value.length <= 0 || faculty.value.length <= 0) {
+    fName.placeholder = 'ФИО студента';
+    dBirth.placeholder = 'Дата рождения';
+    dLearning.placeholder = 'Год начала обучения';
+    faculty.placeholder = 'Факультет';
 
-            }
-            console.log(studentsArray);
-        });
-    };
+    form.classList = "form";
+    fName.classList = "full__name, field";
+    dBirth.classList = "date__birth, field";
+    dLearning.classList = "date__start-training, field";
+    faculty.classList = "faculty, field";
+    button.classList = "button";
 
-    createForm(document.getElementById('formWrapper'))
+    button.textContent = "Добавить студента";
+
+    form.append(fName, dBirth, dLearning, faculty, button);
+    document.getElementById('formWrapper').append(form);
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const fields = form.querySelectorAll('.field')
+      const errors = form.querySelectorAll('.error')
+
+      for (var i = 0; i < errors.length; i++) {
+        errors[i].remove()
+      }
+
+      for (var i = 0; i < fields.length; i++) {
+        if (!fields[i].value) {
+          console.log('field is blank', fields[i])
+          const error = document.createElement('div')
+          error.className = 'error'
+          error.style.color = 'red'
+          error.innerHTML = 'Cannot be blank'
+          form.append(error)
+        }
+      }
+
+    });
+  };
+
+  createForm(document.getElementById('formWrapper'))
 })();
