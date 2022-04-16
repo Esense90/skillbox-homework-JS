f709737e7d2571e409d71460e6f75e869517c2c8
@kirill_riabyshev <kirillryabushev@gmail.com>
2022-04-16 20:06:52 +1100

    hw10

--- 10_type-transform/main1.js
+++ 10_type-transform/main1.js
@@ -2,16 +2,16 @@ let studentsArr = [{
         surname: 'Петров',
         fName: 'Иван',
         patronymic: 'Сергеевич',
-        birthDate: new Date('2000-05-14'),
+        birth: new Date('2000-5-14'),
         sEducation: '2015',
         faculty: 'МКиС',
     },
     {
         surname: 'Кушаков',
-        name: 'Сергей',
+        name: 'Петр',
         patronymic: 'Иванович',
         birth: new Date('2002-7-21'),
-        sEducation: '2021',
+        sEducation: '2017',
         faculty: 'ЖБК',
     },
     {
@@ -19,10 +19,10 @@ let studentsArr = [{
         fName: 'Максим',
         patronymic: 'Олегович',
         birth: new Date('2006-2-26'),
-        sEducation: '2019',
+        sEducation: '2012',
         faculty: 'ТПМ',
     },
-];
+]
 
 function inputsForm() {
     const surname = document.getElementById('surname');
@@ -64,144 +64,29 @@ function addStudent() {
         sEducation: inputForm.sEducation.value.trim(),
         faculty: inputForm.faculty.value.trim()
     };
-
-    studentsArr.push(student);
+    return student;
 };
+let student = addStudent();
 
-function validateForm(student) {
+function studentsTable() {
+    let rowHeader = table.insertRow();
+    for (let prop in studentsArr[0]) {
+        let cell = rowHeader.insertCell();
+        cell.innerText = prop;
+        console.log(prop);
+    }
 
-    let dateArr = String(birth.value.trim());
-
-    for (var i = 0; i < inputForm.fields.length; i++) {
-
-        if (!inputForm.fields[i].value) {
-            inputForm.label[i].innerHTML = 'Заполните поле';
-            inputForm.label[i].classList.add('invalid');
-        } else {
-            inputForm.label[i].innerHTML = '',
-                inputForm.label[i].classList.remove('invalid')
-        };
-        if (new Date(dateArr) <= new Date('01-01-1900')) {
-            document.querySelector('label[for="birth"]').innerHTML = 'Дата от 01-01-1900 до сегодня';
-            document.querySelector('label[for="birth"]').classList.add('invalid');
-        } else if (inputForm.sEducation.value < 2000) {
-            document.querySelector('label[for="sEducation"]').innerHTML = 'Год от 2000 до сегодня';
-            document.querySelector('label[for="sEducation"]').classList.add('invalid');
-        } else if (inputForm.sEducation.value > new Date().getFullYear()) {
-            document.querySelector('label[for="sEducation"]').innerHTML = 'Год от 2000 до сегодня';
-            document.querySelector('label[for="sEducation"]').classList.add('invalid');
+    for (let i = 0; i < studentsArr.length; i++) {
+        let row = table.insertRow();
+        for (let prop in studentsArr[i]) {
+            let cell = row.insertCell();
+            cell.innerText = studentsArr[i][prop];
         }
     }
-
-    if (document.querySelector('label[for="surname"]').classList.contains('invalid')) {
-
-    } else if (document.querySelector('label[for="fName"]').classList.contains('invalid')) {
-
-    } else if (document.querySelector('label[for="patronymic"]').classList.contains('invalid')) {
-
-    } else if (document.querySelector('label[for="birth"]').classList.contains('invalid')) {
-
-    } else if (document.querySelector('label[for="sEducation"]').classList.contains('invalid')) {
-
-    } else if (document.querySelector('label[for="faculty"]').classList.contains('invalid')) {
-
-    } else(addStudent());
-    console.log(studentsArr)
 };
+studentsTable();
 
 
-function addStudentToTable(student, studentsArr) {
+btnSub.addEventListener('click', () => {
 
-    const StudyYears = 4;
-
-    let course = (year) => {
-        let nowYear = new Date().getFullYear();
-        let nowMonth = new Date().getMonth() + 1;
-        if (nowYear - year > 4 || (nowYear - year === StudyYears && nowMonth > 8)) {
-            return 'закончил';
-        } else {
-            return `${nowYear - year} курс`;
-        }
-    }
-
-    let birth = () => {
-        let birth = new Date(student.birth);
-        let year = birth.getFullYear();
-        let month = birth.getMonth() + 1 < 10 ?
-            '0' + String(birth.getMonth() + 1) :
-            birth.getMonth() + 1;
-
-        let day = birth.getDate() < 10 ?
-            '0' + String(birth.getDate()) :
-            birth.getDate();
-
-        let getAge = (birth) => {
-            let now = new Date();
-            let age = now.getFullYear() - birth.getFullYear();
-            return now.setFullYear(1972) < birth.setFullYear(1972) ? age - 1 : age;
-        };
-        return `${day}.${month}.${year} (${getAge(birth)} лет)`;
-    }
-
-    let tr = document.createElement('tr');
-    let tbody = document.querySelector('tbody');
-
-    let newStudent = {
-        fullName: `${student.surname} ${student.fName} ${student.patronymic}`,
-        birth: birth(),
-        startDate: `${student.sEducation} - ${Number(student.sEducation) + StudyYears} (${course(student.sEducation)})`,
-        faculty: student.faculty
-    };
-
-    for (let data of Object.values(newStudent)) {
-        let td = document.createElement('td');
-        td.textContent = data;
-        tr.append(td);
-        tbody.append(tr);
-    }
-    return tr;
-};
-
-const createTableBody = (students) => {
-
-    if (document.querySelector('.tbody')) {
-        tbody = document.querySelector('.tbody')
-        tbody.innerHTML = '';
-    } else {
-        tbody = document.createElement('tbody');
-        tbody.classList.add('tbody');
-    }
-
-    for (const student of students) {
-        row = addStudentToTable(student);
-        tbody.append(row);
-    }
-}
-createTableBody(studentsArr);
-
-function sortArr() {
-
-    let sortBtn = document.querySelectorAll('.sort');
-
-    sortBtn.forEach(function(btnClick) {
-
-        btnClick.addEventListener('click', () => {
-
-            let sortedRows = Array.from(inputForm.table.rows)
-                .slice(1)
-                .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
-
-            inputForm.table.tBodies[0].append(...sortedRows);
-        })
-    })
-}
-sortArr();
-
-btnSub.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    validateForm();
-
-
-
-});
+})
