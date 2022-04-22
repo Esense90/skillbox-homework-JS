587a348df052db99d903fe713c855afe30b0accf
@kirill_riabyshev <kirillryabushev@gmail.com>
2022-04-22 20:02:57 +1100

    hw11

--- 11_client-server/blog-server/main.js
+++ 11_client-server/blog-server/main.js
@@ -1,6 +1,45 @@
-async function loadTodoItems() {
-    const response = await fetch('https://gorest.co.in/public-api/posts?');
-    const data = await response.json();
-    console.log(data);
+const containerBlogs = document.getElementById('containerBlogs');
+const containerPosts = document.getElementById('containerPosts');
+const listsPages = document.getElementById('list__pages');
+
+
+const loadBlogs = async() => {
+    const pageParams = new URLSearchParams(location.search);
+    const postPages = pageParams.get('page');
+
+    const response = await fetch(`https://gorest.co.in/public-api/posts?page=${postPages == null ? 1 : postPages}`);
+    const result = await response.json();
+
+    //  console.log(result);
+    return {
+        posts: result.data,
+        pagination: result.meta.pagination,
+    };
+};
+
+const createPosts = async() => {
+    const pages = await loadBlogs();
+    let listPages = '';
+
+    for (let i = 1; i <= pages.pagination.pages; i++) {
+
+        listPages += `
+        <li class="page-item">
+             <a class="page-link" href="https://gorest.co.in/public-api/posts?page=${i}">
+                 Стр - ${i}
+             <a/>
+        </li>`;
+    }
+    listsPages.innerHTML = listPages;
 }
-loadTodoItems()
+createPosts()
+
+const createBlogs = async() => {
+    const blogs = await loadBlogs();
+
+
+    console.log(blogs.posts)
+
+
+}
+createBlogs()
